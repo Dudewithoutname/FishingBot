8cc6f60a3fff2af259d949297188cfb7eeeee495
Dudewithoutname <54103088+Dudewithoutname@users.noreply.github.com>
2021-12-23 13:49:52 +0100

    Update itemManager.ts

--- src/Items/itemManager.ts
+++ src/Items/itemManager.ts
@@ -4,7 +4,7 @@ import rawItems from './Items.json';
 export class ItemManager{
     public static Items : Item[] = rawItems as Item[];
     
-    static GetItem = (itemId : number) : Item => { 
+    public static GetItem = (itemId : number) : Item => { 
         return this.Items.filter((item: Item) => item.id == itemId)[0];
     }
 }
